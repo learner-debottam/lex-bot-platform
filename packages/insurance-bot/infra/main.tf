593c0a6ff9f42299d08814e5df80c57715efd3d1
Debottam Barman Roy <learner.debottam@gmail.com>
2026-03-28 19:30:56 +0000

    fix: Pipleline issue

--- packages/insurance-bot/infra/main.tf
+++ packages/insurance-bot/infra/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "=> 6.20"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = "=> 6.20"
+  #   }
+  # }
 }
 locals {
   # Load bot configuration
