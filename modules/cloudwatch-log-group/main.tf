1e911b70639a6a415a0b7a3495eef48988cb492b
Debottam Barman Roy <learner.debottam@gmail.com>
2026-03-30 08:01:19 +0100

    fixed: Fixed the pipeline

--- modules/cloudwatch-log-group/main.tf
+++ modules/cloudwatch-log-group/main.tf
@@ -29,7 +29,7 @@
 resource "aws_cloudwatch_log_group" "protected" {
   count             = var.prevent_destroy ? 1 : 0
   name              = var.name
-  retention_in_days = var.retention_in_days
+  retention_in_days = var.retention_in_days != null ? var.retention_in_days : 365
   kms_key_id        = var.kms_key_id != null ? var.kms_key_id : null
   tags              = var.tags
   lifecycle {
